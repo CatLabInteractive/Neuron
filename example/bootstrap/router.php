6b796452c0cf1a2adc6c350d2713ea29799ead68
Thijs Van der Schaeghe <thijs@catlab.be>
2014-11-30 19:09:15 +0100

    Adding tokenized collection.

--- example/bootstrap/router.php
+++ example/bootstrap/router.php
@@ -6,4 +6,8 @@ $router = new \Neuron\Router ();
 $router->get ('/', '\Example\Controllers\HomeController@main');
 $router->get ('/templates', '\Example\Controllers\HomeController@templates');
 
+$router->get ('/test/{something}', function ($a) {
+	return \Neuron\Net\Response::json ($a);
+});
+
 return $router;
