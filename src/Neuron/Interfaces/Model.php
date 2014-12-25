cf502ec5e6e2a27e36691187ae2f144b81415841
Thijs Van der Schaeghe <thijs@catlab.be>
2014-12-25 16:33:31 +0100

    Adding getId Model Interface

---
+++ src/Neuron/Interfaces/Model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daedeloth
+ * Date: 25/12/14
+ * Time: 16:33
+ */
+
+namespace Neuron\Interfaces;
+
+
+interface Model {
+
+	public function getId ();
+
+}
