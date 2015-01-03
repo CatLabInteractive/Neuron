4a58fec2ef9bc0d7e9426ed9989aa30e543f072d
Thijs Van der Schaeghe <thijs@catlab.be>
2015-01-03 15:31:26 +0100

    Adding normalize & partify to URLBuilder.

--- src/Neuron/URLBuilder.php
+++ src/Neuron/URLBuilder.php
@@ -11,7 +11,7 @@ namespace Neuron;
 
 class URLBuilder 
 {
-	public static function getURL ($module = '', $data = array ())
+	public static function getURL ($module = '', $data = array (), $normalize = true)
 	{
 		if (substr ($module, 0, 1) === '/')
 		{
@@ -27,12 +27,48 @@ class URLBuilder
 
 		if (!empty ($params))
 		{
-			return Config::get ('app.url', '/') . $module . '?' . $params;
+			if ($normalize)
+				$url = self::normalize (Config::get ('app.url', '/') . $module) . '?' . $params;
+			else
+				$url = Config::get ('app.url', '/') . $module . '?' . $params;
+
+			return $url;
 		}
 		else
 		{
 			// Google likes these.
-			return Config::get ('app.url', '/') . $module;
+			if ($normalize)
+				return self::normalize (Config::get ('app.url', '/') . $module);
+			else
+				return Config::get ('app.url', '/') . $module;
 		}
+	}
+
+	/**
+	 * Make sure the string does not end with a /
+	 * @param $path
+	 * @return string
+	 */
+	public static function normalize ($path)
+	{
+		if (substr ($path, 0, -1) === '/')
+		{
+			return substr ($path, 0, -1);
+		}
+		return $path;
+	}
+
+	/**
+	 * Make sure that the string ends with a slash.
+	 * @param string $path
+	 * @return string
+	 */
+	public static function partify ($path)
+	{
+		if (substr ($path, -1) !== '/')
+		{
+			return $path . '/';
+		}
+		return $path;
 	}
 }
