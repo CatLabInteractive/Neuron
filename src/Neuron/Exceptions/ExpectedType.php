37338c376c8f2d2b6be2f9be7813c825bb074bec
Thijs Van der Schaeghe <thijs@catlab.be>
2015-01-25 20:25:27 +0100

    Adding a check method to ExpectedType.

--- src/Neuron/Exceptions/ExpectedType.php
+++ src/Neuron/Exceptions/ExpectedType.php
@@ -7,6 +7,16 @@ namespace Neuron\Exceptions;
 class ExpectedType
 	extends InvalidParameter
 {
+	/**
+	 * @param $obj
+	 * @param $className
+	 * @throws ExpectedType
+	 */
+	public static function check ($obj, $className)
+	{
+		if (! ($obj instanceof $className))
+			throw new self ($className);
+	}
 
 	public function __construct ($class)
 	{
