68f604687e2b10f3a8c403e6600e9d5d82dc045d
Thijs Van der Schaeghe <thijs@catlab.be>
2021-02-28 01:36:14 +0100

    Add array of tracking parameters.

---
+++ src/Neuron/Net/QueryTrackingParameters.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Neuron\Net;
+
+/**
+ * Class QueryTrackingParameters
+ * @package Neuron\Net
+ */
+class QueryTrackingParameters
+{
+	/**
+	 * @return QueryTrackingParameters
+	 */
+	public function instance()
+	{
+		static $in;
+		if (!isset($in)) {
+			$in = new self();
+		}
+		return $in;
+	}
+
+	public $queryParameters = [
+		'utm_abversion',
+		'utm_referrer',
+		'utm_source',
+		'utm_medium',
+		'utm_campaign',
+		'utm_term',
+		'utm_content',
+		'_ga'
+	];
+}
